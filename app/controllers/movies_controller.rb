2fa6a2014bc73bcbebda009e3b077e3c15478db7
dheeraj9012 <dheeraj9012@gmail.com>
2015-09-23 22:43:10 -0300

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,7 +38,7 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
+    flash.keep[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
@@ -49,14 +49,14 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
+    flash.keep[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
 
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    flash.keep[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
 
